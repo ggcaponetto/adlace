a4792c59fb016fc40efe1dfec263a03ca8d29524
Giuseppe Caponetto <giuseppe.caponetto@geoimpact.ch>
2022-10-14 00:12:32 +0200

    wip

--- frontend/adlace-builder/src/components/builder/Builder.js
+++ frontend/adlace-builder/src/components/builder/Builder.js
@@ -59,10 +59,14 @@ import {
     NativeScripts
 } from "@emurgo/cardano-serialization-lib-asmjs"
 import * as CSL from "@emurgo/cardano-serialization-lib-asmjs";
-import BufferLib from "buffer/index";
+// import BufferLib from "buffer/index";
 import axios from "axios";
+import * as bip39 from 'bip39';
+import {Transform} from "stream";
 
-const Buffer = BufferLib.Buffer;
+global.Transform = Transform;
+/*const Buffer = BufferLib.Buffer;
+window.Buffer = Buffer;*/
 
 const Bech32Prefix = Object.freeze({
     ADDRESS: 'addr',
@@ -418,71 +422,277 @@ function Builder() {
     );
 }
 
+function Blockfrost(options = {
+    baseUrl: process.env.REACT_APP_BLOCKFROST_BASE_URL_KEY,
+    project_id: process.env.REACT_APP_BLOCKFROST_API_KEY
+}) {
+    this.options = options;
+}
+Blockfrost.prototype.getProtocolParameters = async function (epoch) {
+    return axios({
+        method: "get",
+        headers: {
+            "Content-Type": "application/json",
+            "project_id": this.options.project_id
+        },
+        url: `${this.options.baseUrl}/api/v0/epochs/${epoch}/parameters`
+    })
+};
+Blockfrost.prototype.getAddress = async function (address) {
+    return axios({
+        method: "get",
+        headers: {
+            "Content-Type": "application/json",
+            "project_id": this.options.project_id
+        },
+        url: `${this.options.baseUrl}/api/v0/addresses/${address}`
+    })
+};
+Blockfrost.prototype.getAddressUTXOs = async function (address) {
+    return axios({
+        method: "get",
+        headers: {
+            "Content-Type": "application/json",
+            "project_id": this.options.project_id
+        },
+        url: `${this.options.baseUrl}/api/v0/addresses/${address}/utxos`
+    })
+};
+Blockfrost.prototype.getTransactionMetadataLabels = async function (qs) {
+    return axios({
+        method: "get",
+        headers: {
+            "Content-Type": "application/json",
+            "project_id": this.options.project_id
+        },
+        url: `${this.options.baseUrl}/api/v0/metadata/txs/labels${qs}`
+    })
+};
+Blockfrost.prototype.getTransactionMetadataContentJSON = async function (label, qs) {
+    return await axios({
+        method: "get",
+        headers: {
+            "Content-Type": "application/json",
+            "project_id": this.options.project_id
+        },
+        url: `${this.options.baseUrl}/api/v0/metadata/txs/labels/${label}${qs}`
+    })
+        .then(res => {
+            return res;
+        }).catch(e => {
+            console.error(e);
+        })
+};
+Blockfrost.prototype.getLatestBlock = async function () {
+    return await axios({
+        method: "get",
+        headers: {
+            "Content-Type": "application/json",
+            "project_id": this.options.project_id
+        },
+        url: `${this.options.baseUrl}/api/v0/blocks/latest`
+    })
+        .then(res => {
+            return res;
+        }).catch(e => {
+            console.error(e);
+        })
+};
+
 export function NewBuilder() {
     const [lines, setLines] = useState([
-        <h2>tx builder</h2>
+        <h2>tx builder (using blockfrost: {process.env.REACT_APP_BLOCKFROST_API_KEY})</h2>
     ]);
+
     const log = line => setLines(p => {
         return [...p, line];
     });
-    useEffect(() => {
-        log(<div>tx builder started...</div>)
-        let protocolParameters = {
-            linearFee: {
-                minFeeA: "44",
-                minFeeB: "155381",
-            },
-            minUtxo: "34482",
-            poolDeposit: "500000000",
-            keyDeposit: "2000000",
-            maxValSize: 5000,
-            maxTxSize: 16384,
-            priceMem: 0.0577,
-            priceStep: 0.0000721,
-            coinsPerUtxoWord: "34482",
-        };
-        log(<div>---------------- create the private key ----------------</div>)
-        const privateKey = CSL.PrivateKey.generate_ed25519();
-        log(<div>privateKey (to_bech32): {privateKey.to_bech32()}</div>)
+    function harden(num) {
+        return 0x80000000 + num;
+    }
+    useEffect( () => {
+        const buildNFT = async () => {
+            // collect data from the blockfrost backend
+            const blockfrost = new Blockfrost({
+                baseUrl: process.env.REACT_APP_BLOCKFROST_BASE_URL_KEY,
+                project_id: process.env.REACT_APP_BLOCKFROST_PROJECT_ID
+            });
+            let latestBlock = await blockfrost.getLatestBlock();
+            let parameters = await blockfrost.getProtocolParameters(latestBlock.data.epoch);
 
-        log(<div>---------------- create the private key for the policy ----------------</div>)
-        let policyPrivateKey = CSL.PrivateKey.generate_ed25519()
-        log(<div>policyPrivateKey (to_bech32): {policyPrivateKey.to_bech32()}</div>)
-        log(<div>policyPrivateKey (to_public, to_bech32): {policyPrivateKey.to_public().to_bech32()}</div>)
-        log(<div>policyPrivateKey (to_hex): {policyPrivateKey.to_hex()}</div>)
-        log(<div>---------------- create the policy with the keys above ----------------</div>)
+            log(<div>tx builder started...</div>)
+            let protocolParameters = {
+                linearFee: {
+                    minFeeA: "44",
+                    minFeeB: "155381",
+                },
+                minUtxo: "34482",
+                poolDeposit: "500000000",
+                keyDeposit: "2000000",
+                maxValSize: 5000,
+                maxTxSize: 16384,
+                priceMem: 0.0577,
+                priceStep: 0.0000721,
+                coinsPerUtxoWord: "34482",
+            };
+            log(<div>---------------- create the private key ----------------</div>)
+            // let seed = bip39.generateMnemonic(256);
+            let seed = "auction volume ginger day clean harbor nerve process expect dust elevator park deliver shrug rescue lesson unit wire holiday brother slow captain fade river"
+            log(<div>seed: {seed}</div>)
 
-        const policyPubKey = policyPrivateKey.to_public();
-        const policyAddress = CSL.BaseAddress.new(
-            CSL.NetworkInfo.mainnet().network_id(),
-            CSL.StakeCredential.from_keyhash(policyPubKey.hash()),
-            CSL.StakeCredential.from_keyhash(policyPubKey.hash())
-        ).to_address();
-        log(<div>policyAddress: {policyAddress.to_bech32()}</div>)
+            const entropyPrivateKey = bip39.mnemonicToEntropy(
+                [ "test", "walk", "nut", "penalty", "hip", "pave", "soap", "entry", "language", "right", "filter", "choice" ].join(' ')
+            );
+            const privateKey = CSL.Bip32PrivateKey.from_bip39_entropy(
+                Buffer.from(entropyPrivateKey, 'hex'),
+                Buffer.from(''),
+            );
 
-        const address = CSL.BaseAddress.new(
-            CSL.NetworkInfo.testnet().network_id(),
-            CSL.StakeCredential.from_keyhash(privateKey.to_public().hash()),
-            CSL.StakeCredential.from_keyhash(privateKey.to_public().hash())
-        ).to_address();
-        log(<div>address: {address.to_bech32()}</div>)
+            const accountKey = privateKey
+                .derive(harden(1852)) // purpose
+                .derive(harden(1815)) // coin type
+                .derive(harden(0)); // account #0
 
-        const txBuilder = CSL.TransactionBuilder.new(
-            CSL.TransactionBuilderConfigBuilder.new()
-                .fee_algo(
-                    CSL.LinearFee.new(BigNum.from_str(protocolParameters.linearFee.minFeeA),
-                    CSL.BigNum.from_str(protocolParameters.linearFee.minFeeB))
-                )
-                .pool_deposit(CSL.BigNum.from_str(protocolParameters.poolDeposit))
-                .key_deposit(CSL.BigNum.from_str(protocolParameters.keyDeposit))
-                .coins_per_utxo_word(CSL.BigNum.from_str(protocolParameters.coinsPerUtxoWord))
-                .max_value_size(protocolParameters.maxValSize)
-                .max_tx_size(protocolParameters.maxTxSize)
-                .prefer_pure_change(true)
-                .build()
-        )
-        log(<div>txBuilder is ready...</div>)
+            const publicKey = accountKey
+                .derive(0) // external
+                .derive(0)
+                .to_public();
 
+            const stakeKey = accountKey
+                .derive(2) // chimeric
+                .derive(0)
+                .to_public();
+
+            const addressPool = [];
+            for(let i = 0; i < 10; i++){
+                const tempPublicKey = accountKey
+                    .derive(0) // external
+                    .derive(i)
+                    .to_public();
+                const address = CSL.BaseAddress.new(
+                    CSL.NetworkInfo.mainnet().network_id(),
+                    CSL.StakeCredential.from_keyhash(tempPublicKey.to_raw_key().hash()),
+                    CSL.StakeCredential.from_keyhash(tempPublicKey.to_raw_key().hash())
+                ).to_address();
+                addressPool.push(address)
+            }
+
+            // const privateKey = CSL.PrivateKey.generate_ed25519();
+            log(<div>privateKey (to_bech32): {privateKey.to_bech32()}</div>)
+            log(<div>publicKey (to_bech32): {publicKey.to_bech32()}</div>)
+            log(<div>stakeKey (to_bech32): {stakeKey.to_bech32()}</div>)
+            log(<div>addressPool (to_bech32): {addressPool.map(a => a.to_bech32()).join("\n")}</div>)
+
+            log(<div>---------------- create the private key for the policy ----------------</div>)
+            // let seedPolicy = bip39.generateMnemonic(256);
+            let seedPolicy = "hungry miss kangaroo pattern ghost derive pluck hunt recipe tank thank ensure quick install addict total announce lyrics resource arrow bus wagon aerobic timber";
+            log(<div>seedPolicy: {seedPolicy}</div>)
+            const entropyPolicyPrivateKey = bip39.mnemonicToEntropy(seedPolicy);
+            const policyPrivateKey = CSL.Bip32PrivateKey.from_bip39_entropy(
+                Buffer.from(entropyPolicyPrivateKey, 'hex'),
+                Buffer.from(''),
+            );
+
+            const policyAccountKey = privateKey
+                .derive(harden(1852)) // purpose
+                .derive(harden(1815)) // coin type
+                .derive(harden(0)); // account #0
+
+            const policyPublicKey = accountKey
+                .derive(0) // external
+                .derive(0)
+                .to_public();
+
+            const policyStakeKey = accountKey
+                .derive(2) // chimeric
+                .derive(0)
+                .to_public();
+            // let policyPrivateKey = CSL.PrivateKey.generate_ed25519()
+            log(<div>policyAccountKey (to_bech32): {policyAccountKey.to_bech32()}</div>)
+            log(<div>policyPublicKey (to_bech32): {policyPublicKey.to_bech32()}</div>)
+            log(<div>policyStakeKey (to_bech32): {policyStakeKey.to_bech32()}</div>)
+            log(<div>---------------- create the policy with the keys above ----------------</div>)
+
+            const policyPubKey = policyPrivateKey.to_public();
+            const policyAddress = CSL.BaseAddress.new(
+                CSL.NetworkInfo.mainnet().network_id(),
+                CSL.StakeCredential.from_keyhash(policyPubKey.to_raw_key().hash()),
+                CSL.StakeCredential.from_keyhash(policyPubKey.to_raw_key().hash())
+            ).to_address();
+            log(<div>policyAddress: {policyAddress.to_bech32()}</div>)
+
+            const address = CSL.BaseAddress.new(
+                CSL.NetworkInfo.mainnet().network_id(),
+                CSL.StakeCredential.from_keyhash(publicKey.to_raw_key().hash()),
+                CSL.StakeCredential.from_keyhash(publicKey.to_raw_key().hash())
+            ).to_address();
+            log(<div>address: {address.to_bech32()}</div>)
+
+            const ttl = latestBlock.data.slot + 60 * 60 * 2;  // two hours from now
+            const txBuilder = CSL.TransactionBuilder.new(
+                CSL.TransactionBuilderConfigBuilder.new()
+                    .fee_algo(
+                        CSL.LinearFee.new(BigNum.from_str(parameters.data.min_fee_a.toString()),
+                            CSL.BigNum.from_str(parameters.data.min_fee_b.toString()))
+                    )
+                    .pool_deposit(CSL.BigNum.from_str(parameters.data.pool_deposit))
+                    .key_deposit(CSL.BigNum.from_str(parameters.data.key_deposit))
+                    .coins_per_utxo_word(CSL.BigNum.from_str(parameters.data.coins_per_utxo_word))
+                    .max_value_size(parameters.data.max_val_size)
+                    .max_tx_size(parameters.data.max_tx_size)
+                    .prefer_pure_change(true)
+                    .build()
+            )
+            log(<div>txBuilder is ready...</div>)
+
+            const scripts = CSL.NativeScripts.new();
+
+            const policyKeyHash = CSL.BaseAddress.from_address(policyAddress)
+                .payment_cred()
+                .to_keyhash();
+
+            log(<div>the policy keyhash is: ${Buffer.from(policyKeyHash.to_bytes()).toString("hex")}</div>);
+
+            // add key hash script so only people with policy key can mint assets using this policyId
+            const keyHashScript = CSL.NativeScript.new_script_pubkey(
+                CSL.ScriptPubkey.new(policyKeyHash)
+            );
+            scripts.add(keyHashScript);
+
+            const policyTtl = ttl;
+
+            log(<div>the policy time to live is: {policyTtl}</div>);
+
+            // add timelock so policy is locked after this slot
+            const timelock = CSL.TimelockExpiry.new(policyTtl);
+            const timelockScript = CSL.NativeScript.new_timelock_expiry(timelock);
+            scripts.add(timelockScript);
+
+            const mintScript = CSL.NativeScript.new_script_all(
+                CSL.ScriptAll.new(scripts)
+            );
+
+            // select the utxo to use as input
+            let utxos = await blockfrost.getAddressUTXOs(address.to_bech32()).catch(e => {
+                return e.response;
+            });
+            let utxo = null;
+            if (utxos.data && utxos.data.status_code === 200) {
+                for (const utxoEntry of utxos.data) {
+                    if (utxoEntry.amount > 3000000) {
+                        utxo = utxoEntry;
+                    }
+                }
+            }
+
+            if (utxo === null) {
+                log(<div>selected the utxo: no valid utxo found</div>);
+                // throw new Error("no utxo found with sufficient ADA.");
+            } else {
+                log(<div>selected the utxo: {JSON.stringify(utxo)}</div>);
+            }
+        }
+        buildNFT();
     }, [])
     return (
         <div>
